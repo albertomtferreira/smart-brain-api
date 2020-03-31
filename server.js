1f567ab7a912d7405818ad36d52f7b66e920ddb4
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-03-31 17:17:42 +0100

    fist upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const app = express();
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const imageCount = require('./controllers/imageCount');
+
+const db = knex ({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'Alberto',
+      password : 'AlbTao01',
+      database : 'smart-brain'
+    }
+});
+
+app.use(express.json());
+app.use(cors());
+
+// ROUTE
+app.get('/', (req, res) => {res.send(database.users)})
+//SIGNIN
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+//REGISTER
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+//PROFILE ID
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+//IMAGE COUNT
+app.put('/image', (req, res) => {imageCount.handleImageCount(req, res, db)})
+
+app.post('/imageurl', (req, res) => {imageCount.handleApiCall(req, res)})
+
+app.listen(3001, ()=> {console.log('app is running on port 3001')})
+
+
+/*
+/ --> res = this is working
+/signin --> Post = Success/Fail
+/Register --> Post = user
+/profile/:userID --> GET = user
+/image --> PUT --> user
+*/
