478c879db0222514718c4815bfd14c1312d12d7e
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-01 17:54:35 +0100

    ff

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,6 @@ const handleRegister = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
        })
-       
         .catch(err => res.status(400).json('Unable to register Line 30 - Register.js'));
 }
 module.exports = {
