24737333fd274782c08b9d505b6adea649b1b563
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-03 10:38:04 +0100

    api

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-//const API = process.env.API;
+const API = process.env.API;
 const db = knex({
   client: 'pg',
   connection: {
@@ -23,11 +23,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is running') });
+app.get('/', (req, res)=> { res.send('it is running',`API ${API}`)});
 app.post('/signin', signin.handleSignin(db, bcryptNodeJs));
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcryptNodeJs)});
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
+
 
 app.listen(process.env.PORT || 3000, ()=> {console.log(`app is running on port ${process.env.PORT} `)});
