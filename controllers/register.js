c5283cb28cb964572842826d5cb34731b9d8c54a
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-03 14:35:02 +0100

    undo security update

--- controllers/register.js
+++ controllers/register.js
@@ -1,38 +1,33 @@
-const handleRegister = (req, res, db, bcrypt, saltRounds) => {
+const handleRegister = (req, res, db, bcryptNodeJs) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   } 
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-      bcrypt.hash(Password, salt, function(err, hash) {
-          // Store hash in your password DB.
-          // const hash = bcrypt.hashSync(password);
-          db.transaction(trx => {
-          trx.insert({
-              hash: hash,
-              email: email
+  const hash = bcryptNodeJs.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
           })
-          .into('login')
-          .returning('email')
-          .then(loginEmail => {
-              return trx('users')
-              .returning('*')
-              .insert({
-                  email: loginEmail[0],
-                  name: name,
-                  joined: new Date()
-              })
-              .then(user => {
-                  res.json(user[0]);
-              })
+          .then(user => {
+            res.json(user[0]);
           })
-          .then(trx.commit)
-          .catch(trx.rollback)
-          })
-          .catch(err => res.status(400).json(`Unable to register `,'\n',`Error: ${err}`))
-      }); 
-  });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json(`Unable to register `,'\n',`Error: ${err}`))
 }
 module.exports = {
-handleRegister
+  handleRegister
 };
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcryptNodeJs = require('bcrypt-nodejs');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 const cors = require('cors');
 const knex = require('knex');
-
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
-
 
 const db = knex({
   client: 'pg',
@@ -22,12 +18,10 @@ const db = knex({
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
 app.get('/', (req, res)=> {res.send(`it is running`)});
 app.post('/signin', signin.handleSignin(db, bcryptNodeJs));
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds)});
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcryptNodeJs)});
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => { image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)});
-
 app.listen(process.env.PORT || 3000, ()=> {console.log(`app is running on port ${process.env.PORT} `)});
